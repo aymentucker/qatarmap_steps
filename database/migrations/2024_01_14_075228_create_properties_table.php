1fde47a588d3609898d30f6e5c1cdd146f563f63
Aymen Tucker <sheildlab@gmail.com>
2024-01-21 14:26:39 +0300

    Update 2024_01_14_075228_create_properties_table.php

--- database/migrations/2024_01_14_075228_create_properties_table.php
+++ database/migrations/2024_01_14_075228_create_properties_table.php
@@ -28,13 +28,20 @@ return new class extends Migration
             $table->decimal('property_area', 10, 2);
             $table->decimal('price', 10, 2);
             $table->text('description');
-            // $table->text('pictures'); // JSON format or string for single picture
             $table->string('status');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
 
+        });
+        Schema::create('property_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('property_image_id');
+            $table->string('url');
+            $table->timestamps();
+    
+            $table->foreign('property_image_id')->references('id')->on('properties');
         });
     }
 
@@ -44,5 +51,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('properties');
+        Schema::dropIfExists('property_images');
     }
 };
